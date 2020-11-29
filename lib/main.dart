f89ae2acb94dd9f75b1ae7aee976da1ca1861225
ZTW21 <ZTWilson2002@gmail.com>
2020-11-28 19:35:47 -0500

    Custom Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,17 @@ class MyApp extends StatelessWidget {
                   fontSize: 40.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
+                  fontFamily: 'Castoro',
+                ),
+              ),
+              Text(
+                'University Student and Developer',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.blueGrey[100],
+                  fontFamily: 'SourceSansPro',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
                 ),
               ),
             ],
